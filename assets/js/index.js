4c1a3a883f08fdecc3829a4adadf550055df56e9
Daniel Torres <torres.daniel.0827@gmail.com>
2024-06-12 17:48:47 -0500

    First Commit

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+const searchedEl = document.getElementById("searched-cities");
+
+function init () {
+    const searchedLocations = JSON.parse(localStorage.getItems('locations'));
+
+    if(searchedLocations.length === 0) {
+        return;
+    }
+
+    for(const location of searchedLocations){
+    }
+}
+
+init()
