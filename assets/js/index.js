90092c2e93e2312bb4b3906545d53758e954e102
Daniel Torres <torres.daniel.0827@gmail.com>
2024-06-13 16:50:42 -0500

    added functionality to initialize dashboard and add city to search history list

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,25 @@
-const searchedEl = document.getElementById("searched-cities");
+const API_KEY = '5be4d368ce2667761b6edd7cd8dd4895';
+const searchBtn = document.getElementById('searchBtn');
+const cityInput = document.getElementById('city');
+const searchedEl = document.getElementById('searched-cities');
+const currentWeatherEl = document.getElementById('current-weather');
+const forecastEl = document.getElementById('forecast');
 
-function init () {
-    const searchedLocations = JSON.parse(localStorage.getItems('locations'));
-
-    if(searchedLocations.length === 0) {
-        return;
+function init() {
+  const searchedLocations = JSON.parse(localStorage.getItem('locations')) || [];
+  if (searchedLocations.length > 0) {
+    for (const location of searchedLocations) {
+      addCityToHistory(location);
     }
+  }
+}
 
-    for(const location of searchedLocations){
-    }
+function addCityToHistory(city) {
+  const cityButton = document.createElement('button');
+  cityButton.textContent = city;
+  cityButton.classList.add('btn', 'btn-secondary', 'btn-block', 'mt-2');
+  cityButton.addEventListener('click', () => getWeatherData(city));
+  searchedEl.appendChild(cityButton);
 }
 
 init()
